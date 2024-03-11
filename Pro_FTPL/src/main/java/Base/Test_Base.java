85131a6af015803b565d0f0fe618b3d7e3f9a286
shubhamupare01 <162920275+shubhamupare01@users.noreply.github.com>
2024-03-11 11:21:43 +0530

    change01

--- Pro_FTPL/src/main/java/Base/Test_Base.java
+++ Pro_FTPL/src/main/java/Base/Test_Base.java
@@ -3,7 +3,6 @@ package Base;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
